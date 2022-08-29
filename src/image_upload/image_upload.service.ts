f6058153b821e76928f465bb5ef87b3d383f6550
nishit.m.vankawala <nishit.m.vankawala@doyenhub.com>
2022-08-29 16:22:38 +0530

    Added validation and resize logic

--- src/image_upload/image_upload.service.ts
+++ src/image_upload/image_upload.service.ts
@@ -4,18 +4,29 @@ import * as AWS from 'aws-sdk';
 import s3Storage = require("multer-sharp-s3");
 import config from "../config/config";
 
-const AWS_S3_BUCKET_NAME = config.AWS_S3_BUCKET_NAME || 'doyenhub-demo';
+// AWS config used in Multer
+const AWS_S3_BUCKET_NAME = config.AWS_S3_BUCKET_NAME || 'user-for-interview-test-cases';
 
 AWS.config.update({
   accessKeyId: config.AWS_ACCESS_KEY_ID,
   secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
 });
 
-const s3 = new AWS.S3();
+const s3 = new AWS.S3(); // S3 bucket Object
 
 @Injectable() 
 export class ImageUploadService {
   constructor() {}
+
+  fileFilter = (_req, file, cb) => {
+    cb(null, true);
+    // Only JPG, JPEG and PNG File format allowed
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+      cb(new Error('Invalid file type, only JPG, JPEG and PNG is allowed!'), false);
+    }
+  }
 
   async fileupload(@Req() req, @Res() res) {
     try {
@@ -24,7 +35,8 @@ export class ImageUploadService {
           console.log(error);
           return res.status(404).json(`Failed to upload image file: ${error}`);
         }
-        return res.status(201).json(req.files[0].location);
+        // console.log(req.files);
+        return res.status(201).json(`Image Successfully Stored in S3 Bucket`);
       });
     } catch (error) {
       console.log(error);
@@ -32,33 +44,25 @@ export class ImageUploadService {
     }
   }
 
-
-
   upload = multer({
+    fileFilter: this.fileFilter,
+    limits:{
+      files: 1, // allow only 1 file per request
+      fileSize: 1024 * 1024 * 10 // 10 MB (max file size)
+    },
     storage: s3Storage({
       s3: s3,
       Bucket: AWS_S3_BUCKET_NAME,
       ACL: 'public-read',
       Key: function(request, file, cb) {
-        // Only .png, .jpg and .jpeg format allowed
-        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-          const fileSize = parseInt(request.headers['content-length']);
-          if (fileSize > 1048576) { // File size restricted to 10MB
-            cb(null, false);
-            return cb(new Error('Maximum 10MB image size Allowed!'));
-          } else {
-            cb(null, `${Date.now().toString()} - ${file.originalname}`);
-          }
-        } else {
-          cb(null, false);
-          return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-        }
-        
+        cb(null, `${Date.now().toString()} - ${file.originalname}`);
       },
-      resize: { // we will resize image to 300x300
-        width: 300,
-        height: 300,
-      },
+      multiple: true, // flag required to store multiple images
+      resize: [ // this will resize and store image to 3 images with suffix
+        { suffix: 'xlg', width: 2048, height: 2048 },
+        { suffix: 'lg', width: 1024, height: 1024 },
+        { suffix: 'sm', width: 300, height: 300 },
+      ],
     }),
   }).array('upload', 1);
 }
